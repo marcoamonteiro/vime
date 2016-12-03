86f7636789d203d079cfa5b00ca6b9b345314331
Tobin Bell <trfbell@gmail.com>
2016-12-03 00:56:49 -0800

    More ui and beginning of MVC in vime.

--- vime.c
+++ vime.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include "vime_ui.h"
+#include "buffer.h"
+#include "util.h"
 
 static VimeUI ui;
+static VimeUIDelegate uiDelegate;
+static VimeBuffer buffer;
+
+char *getLine(uint line);
 
 int main(int argc, char *argv[]) {
+    
     if (argc < 2) {
         printf("Usage: vime <filename>\n");
         return 0;
     }
+
+    buffer = VimeBuffer_new();
+    VimeBuffer_readFile(buffer, argv[1]);
     ui = VimeUI_new();
+
+    uiDelegate = VimeUIDelegate_new();
+    uiDelegate->stringForLine = &getLine;
     VimeUI_render(ui);
     VimeUI_dispose(ui);
     return 0;
 }
+
+/*
+ * MARK: Vime UI Delegate Methods
+ */
+
+// Get string for given line number. This returns the string to the UI when
+// it asks for it. It gets the string from the VimeBuf.
+char *getLine(uint line) {
+    return VimeBuffer_getLine(buffer, line);
+}
+
--- vime_ui.h
+++ vime_ui.h
@@ -3,17 +3,32 @@
 
 #include "util.h"
 
+typedef char *(*VimeUILineSource)(uint);
+
+typedef struct VimeUIDelegate {
+    VimeUILineSource stringForLine;
+} *VimeUIDelegate;
+
 typedef struct VimeUI {
     uint line;
     uint column;
+    VimeUIDelegate delegate;
 } *VimeUI;
 
 VimeUI VimeUI_new(void);
 void VimeUI_dispose(VimeUI vui);
 
+void VimeUI_up(VimeUI vui);
+void VimeUI_down(VimeUI vui);
+void VimeUI_left(VimeUI vui);
+void VimeUI_right(VimeUI vui);
+void VimeUI_toLine(VimeUI vui, uint line);
+void VimeUI_toColumn(VimeUI vui, uint column);
+
 void VimeUI_clear(VimeUI vui);
 void VimeUI_render(VimeUI vui);
-void VimeUI_setLine(VimeUI vui, uint line);
-void VimeUI_setColumn(VimeUI vui, uint column);
+
+VimeUIDelegate VimeUIDelegate_new(void);
+void VimeUIDelegate_dispose(VimeUIDelegate d);
 
 #endif
